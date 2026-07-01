716d29f1cfddd97adaae1eb29e65215261522b3d
ArghoPH <arghochakma2023@gmail.com>
2026-07-01 12:30:24 +0600

    refactor: update map.js with additional examples and comments

--- map.js
+++ map.js
@@ -4,3 +4,35 @@
 
 // console.log(newNumbers);
 
+// const numbers = [10, 20, 30];
+
+// const newNumbers = numbers.map(function (number) {
+//     return number;
+// });
+
+// console.log(newNumbers);
+
+
+// array.map(function (item) {
+//     return item;
+// });
+
+// array.map(function (item) {
+//     return item;
+// })
+
+// array.map(function (item) {
+//     return item;
+// })
+
+// array.map(function (item) {
+
+// })
+
+// const numbers = [10, 20, 30];
+
+// const result = numbers.map(function (number) {
+//     return number;
+// })
+
+// console.log(result);
