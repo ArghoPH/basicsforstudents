0606493126ae2f78d7a876158a0f7b7a637381ef
ArghoPH <arghochakma2023@gmail.com>
2026-07-12 13:19:33 +0600

    com

--- map.js
+++ map.js
@@ -900,3 +900,16 @@
 
 
 
+// Expert Bonus
+const advancedOrders = [
+    {
+        id: 7001,
+        trackingCode: "ADV-1001",
+        userId: 101,
+        items: [
+            { productId: 1, quantity: 2 },
+            { productId: 3, quantity: 1 }
+        ],
+        paid: true
+    }
+];
