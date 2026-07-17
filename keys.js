eedebab1ab69cea289668378c5c72676dd98f199
Argho Chakma <arghochakma2023@gmail.com>
2026-07-17 13:42:19 +0600

    com

--- keys.js
+++ keys.js
@@ -2,7 +2,9 @@ const computer = {
     brand: "lenovo",
     price: 35000,
     processor: 'intel',
-    hdd: '512 gb'
+    hdd: '512 gb',
+    stock: 4
+    
 },
 
     keys = Object.keys(computer)
