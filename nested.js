4d9aee8d124fcc9d83e35439eb3ab93ebc129f0c
Argho Chakma <arghochakma2023@gmail.com>
2026-08-13 21:45:19 +0600

    Add new item to the order in nested.js

--- nested.js
+++ nested.js
@@ -5,6 +5,7 @@ const orders = [
         userId: 101,
         items: [
             { productId: 1, quantity: 2 },
+            
             { productId: 3, quantity: 1 }
         ],
         paid: true
